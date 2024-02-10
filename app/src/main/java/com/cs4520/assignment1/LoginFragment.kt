bbc0eb27ec89e2ae23c25f3edda76443110bc0de
Thomas Grbic <tomgrbz@gmail.com>
2024-02-10 15:59:43 -0500

    Acitivites nnow show up in git history

---
+++ app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
@@ -0,0 +1,45 @@
+package com.cs4520.assignment1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlin.math.log
+
+class LoginFragment: Fragment() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.login_fragment, container, false)
+        usernameEditText = view.findViewById(R.id.usernameEditText)
+        passwordEditText = view.findViewById(R.id.passwordEditText)
+        loginButton = view.findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                usernameEditText.text.clear()
+                passwordEditText.text.clear()
+                findNavController().navigate(R.id.action_loginFragment_to_productListFragment)
+            } else {
+                Toast.makeText(requireContext(), "Incorrect username or password", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        return view
+    }
+}
---
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.cs4520.assignment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment())
+                .commit()
+        }
+
+    }
+
+}
